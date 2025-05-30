ebff8227ca10d746f7e7aba3e6e2eb3f063bfc03
Yus314 <shizhaoyoujie@gmail.com>
2025-05-30 15:49:39 +0900

    typeを修正した

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ fn main() {
                         key_code: to_key.to_string(),
                         modifiers: None,
                     },
-                    type_: "basic".to_string(),
+                    r#type: "basic".to_string(),
                 })
                 .collect(),
         }],
